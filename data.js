7a3d24df60968548a30bc67cfc87b84aa648b93d
umutcyln <umutceylan1023@gmail.com>
2026-09-05 00:04:24 +0300

    refactor: Proje data.js tabanlı dinamik mimariye geçirildi, img klasör yapısı kuruldu

---
+++ data.js
@@ -0,0 +1,17 @@
+// Tüm kariyer verilerinin tek merkezden yönetildiği veritabanı
+const kariyerVerileri = [
+    { sezon: "36/37", takim: "Galatasaray", mac: 40, gol: 62, asist: 29, sari: 4, kirmizi: 0, reyting: "-" },
+    { sezon: "35/36", takim: "Galatasaray", mac: 56, gol: 75, asist: 35, sari: 4, kirmizi: 2, reyting: "9.0" },
+    { sezon: "34/35", takim: "Juventus", mac: 68, gol: 98, asist: 43, sari: 10, kirmizi: 0, reyting: "9.7" },
+    { sezon: "33/34", takim: "Juventus", mac: 59, gol: 68, asist: 23, sari: 4, kirmizi: 2, reyting: "9.5" },
+    { sezon: "32/33", takim: "Juventus", mac: 59, gol: 86, asist: 40, sari: 5, kirmizi: 1, reyting: "9.2" },
+    { sezon: "31/32", takim: "Tottenham", mac: 49, gol: 57, asist: 33, sari: 10, kirmizi: 2, reyting: "7.5" },
+    { sezon: "30/31", takim: "Tottenham", mac: 61, gol: 102, asist: 40, sari: 4, kirmizi: 0, reyting: "9.9" },
+    { sezon: "29/30", takim: "Tottenham", mac: 56, gol: 74, asist: 23, sari: 2, kirmizi: 1, reyting: "8.4" },
+    { sezon: "28/29", takim: "Tottenham", mac: 62, gol: 88, asist: 27, sari: 10, kirmizi: 1, reyting: "9.4" },
+    { sezon: "27/28", takim: "Tottenham", mac: 64, gol: 81, asist: 37, sari: 7, kirmizi: 0, reyting: "9.5" },
+    { sezon: "26/27", takim: "Tottenham", mac: 63, gol: 68, asist: 34, sari: 5, kirmizi: 0, reyting: "8.5" },
+    { sezon: "25/26", takim: "Liverpool", mac: 60, gol: 38, asist: 28, sari: 3, kirmizi: 1, reyting: "7.0" },
+    { sezon: "24/25", takim: "Realmadrid", mac: 59, gol: 51, asist: 50, sari: 1, kirmizi: 0, reyting: "9.3" },
+    { sezon: "23/24", takim: "Galatasaray", mac: 48, gol: 35, asist: 25, sari: 2, kirmizi: 0, reyting: "7.5" }
+];
